66c20cadc9693bb1125272d83b71d7db6b488d87
Joao Gabriel <joaobrick17@gmail.com>
2025-03-11 13:31:34 -0300

    Add files via upload

---
+++ exercicio 8 .py
@@ -0,0 +1,4 @@
+salario = 500
+aumento = 150
+print(salario+aumento)
+print(novo_salario)
---
+++ exercicio 9.py
@@ -0,0 +1,16 @@
+velocidadeA = 50
+velocidadeB = 60
+velocidadeC= 70
+velocidadeD= 90
+velocidadeE= 80
+
+
+limite_velocidade=80
+
+print(velocidadeA >= limite_velocidade)
+print(velocidadeB >= limite_velocidade)
+print(velocidadeC >= limite_velocidade)
+print(velocidadeD >= limite_velocidade)
+print(velocidadeE >= limite_velocidade)
+
+ 
